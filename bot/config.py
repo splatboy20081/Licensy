e1265a8ff41f61f6dbe9770855c828ac39c91411
Braindead <albertopoljak@gmail.com>
2020-06-02 01:44:41 +0200

    Updated config.py

--- bot/config.py
+++ bot/config.py
@@ -8,12 +8,15 @@ SUPPORT_CHANNEL_INVITE: Final = "https://discord.gg/trCYUkz"
 PATREON_LINK: Final = "https://www.patreon.com/Licensy"
 TOP_GG_VOTE_LINK: Final = "https://top.gg/bot/604057722878689324"
 SOURCE_CODE_LINK: Final = "https://github.com/albertopoljak/Licensy/"
+FAQ_LINK: Final = "https://github.com/albertopoljak/Licensy/wiki/FAQ"
+QUICKSTART_LINK: Final = "https://github.com/albertopoljak/Licensy#quickstart-bot-usage"
 DEFAULT_PREFIX: Final = "lic!"
 
 DEVELOPER_LOG_CHANNEL_ID: Final = 613847243266719755
+SUGGESTIONS_CHANNEL_ID: Final = 621366699316215828
 GUILD_LOG_CHANNEL_ID: Final = 716792061302407260
 UPDATE_CHANNEL_ID: Final = 625404542535598090
-MAXIMUM_UNUSED_GUILD_LICENSES: Final = 100
+MAXIMUM_UNUSED_GUILD_LICENSES: Final = 100  # Change this then also change FAQ
 MAXIMUM_LICENSE_DURATION_HOURS: Final = 8784  # Represents maximum possible hours (leap year) in 12 months
 
 DEVELOPERS: Final = {
