f0ed90783c90177eb6831ee425e62fea2904a3b9
Braindead <albertopoljak@gmail.com>
2020-06-29 15:59:50 +0200

    Added uvloop and TortoiseORM support to __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,45 +1,28 @@
 import os
+import asyncio
 import logging
-import traceback
 from sys import stdout
-from pathlib import Path
 
 from dotenv import load_dotenv
+from tortoise import Tortoise
 
 from bot import Licensy
+from bot.config import DATABASE_DSN
 
+
+load_dotenv()
 
 root_logger = logging.getLogger()
 root_logger.setLevel(logging.INFO)
-
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(message)s")
-
 console_logger = logging.getLogger("console")
 console = logging.StreamHandler(stdout)
-console.setFormatter(formatter)
+console.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(message)s"))
 console_logger.addHandler(console)
-
 root_logger.addHandler(console)  # TODO temporal for development stage
-
-load_dotenv()
-bot = Licensy()
-
-
-for extension_path in Path("bot/cogs").glob("*.py"):
-    extension_name = extension_path.stem
-    dotted_path = f"bot.cogs.{extension_name}"
-
-    try:
-        bot.load_extension(dotted_path)
-    except Exception as e:
-        traceback_msg = traceback.format_exception(etype=type(e), value=e, tb=e.__traceback__)
-        console_logger.info(f"Failed to load cog {dotted_path} - traceback:{traceback_msg}")
-    else:
-        console_logger.info(f"loaded {dotted_path}")
 
 
 """
-uvloop is a ultra fast asyncio event loop that makes asyncio 2-4x faster.
+uvloop is a fast, drop-in replacement of the built-in asyncio event loop that makes asyncio 2-4x faster.
 It's a optional dependency as it is not supported on Windows.
 """
 try:
@@ -52,4 +35,14 @@ else:
     console_logger.info("uvloop successfully installed.")
 
 
-bot.run(os.getenv("BOT_TOKEN"))
+async def database_init():
+    await Tortoise.init(
+        db_url=DATABASE_DSN,
+        modules={'models': ["bot.models.models"]}
+    )
+    await Tortoise.generate_schemas()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(database_init())
+Licensy(loop=loop).run(os.getenv("BOT_TOKEN"))
